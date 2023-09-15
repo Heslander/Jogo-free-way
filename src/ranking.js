b2fdaada9ec4b35f4e18bef4e891d39d98299fe9
Heslander <heslanderlopes@gmail.com>
2023-09-15 00:15:21 -0300

    codigo do ranking

--- src/ranking.js
+++ src/ranking.js
@@ -1,8 +1,22 @@
 
+let registro = [];
 
 function ranking (){
     document.write = (ident);
     return ident
   }
 
-  ranking(ident);
+  function verificar(){
+    if (ident !== null){
+      registro.push(ident);
+    }
+    else {
+      return "ABC" ;
+    }
+  }
+
+  //function exibirRanking (){
+ //   registro.sort ((a,b) => b.pontuacao - a.pontuacao);
+ // }
+
+  console.log(registro);
